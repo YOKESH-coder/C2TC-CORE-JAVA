4d8d5fc06347cc19fd6570bcfa3c4548b8553218
ELCOT <ELCOT@Lenovo>
2023-02-25 18:59:31 +0400

    first commit

---
+++ datatype/src/datatype/dtype.java
@@ -0,0 +1,18 @@
+package datatype;
+
+public class dtype {
+
+	public static void main(String[] args) {
+		int value1 = 9/2;
+		System.out.println(value1);
+
+		float value2 = 110f/40f;
+		System.out.println(value2);
+		
+		double value3 = 10d/3d;
+		System.out.println(value3);	
+	
+		double value4 = 1.6666d;
+		System.out.print(value4);
+	}
+}
